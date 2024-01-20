c531645e8c766494e6eda6e9866c007d41a33e76
davidcoats <david@getsafetycone.com>
2024-01-20 15:09:42 -0800

    Initial commit.

---
+++ source/R5T.T0241.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0241.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.T0241 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0241/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0241
+{
+	/// <summary>
+	/// Context operations marker attribute and marker interface types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
